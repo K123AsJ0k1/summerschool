13f43480e1d4d94b252b5bbfe6dde01e907fa487
K123AsJ0k1 <niila.siilasjoki@gmail.com>
2025-06-27 18:31:13 +0300

    ex21

--- openmp/hybrid-hello/hello.cpp
+++ openmp/hybrid-hello/hello.cpp
@@ -7,8 +7,8 @@ int main(int argc, char *argv[])
     int rank, thread_id;
     int provided, required=MPI_THREAD_FUNNELED;
 
-    MPI_Init_thread(&argc, &argv, required, &provided)
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank)
+    MPI_Init_thread(&argc, &argv, required, &provided);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     
     #pragma omp parallel private(thread_id)
     {
