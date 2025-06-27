a466ef17fce4b1cbb38d376a408de84605b7defe
K123AsJ0k1 <niila.siilasjoki@gmail.com>
2025-06-27 14:09:06 +0300

    ex18

--- openmp/simple-tasks/tasks.cpp
+++ openmp/simple-tasks/tasks.cpp
@@ -3,24 +3,30 @@
 
 int main(void)
 {
-    int array[4] = {0, 0, 0, 0};
-    int tid;
+     int array[4] = {0, 0, 0, 0};
+     int tid;
 
-    printf("Array at the beginning: ");
-    for (int i=0; i < 4; i++) {
-         printf("%d ", array[i]);
-    }
-    printf("\n");
+     printf("Array at the beginning: ");
+     for (int i=0; i < 4; i++) {
+          printf("%d ", array[i]);
+     }
+     printf("\n");
 
-    // TODO: launch threads and create tasks so that there 
-    // one task per loop iteration 
-    for (int i=0; i < 4; i++) {
-           tid = omp_get_thread_num();
-           printf("Task %d executed by thread %d\n", i, tid);
-           array[i] += tid;
-    }
-
-    // TODO end
+     #pragma omp parallel private(tid)
+          #pragma omp single
+          {
+              tid = omp_get_thread_num(); 
+              printf("Tasks created by %d\n", tid);
+          
+          for (int i=0; i < 4; i++) {
+               #pragma omp task if (i < 3)
+               {
+                    tid = omp_get_thread_num();
+                    printf("Task %d executed by thread %d\n", i, tid);
+                    array[i] += tid;
+               }
+          }
+          }
 
     printf("Array at the end: ");
     for (int i=0; i < 4; i++) {
