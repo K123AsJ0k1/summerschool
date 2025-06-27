92e462ea5277a669fdb80ceacc556c29cf8c22b9
K123AsJ0k1 <niila.siilasjoki@gmail.com>
2025-06-27 09:55:05 +0300

    ex12

--- openmp/work-sharing/addition.cpp
+++ openmp/work-sharing/addition.cpp
@@ -1,4 +1,5 @@
 #include <cstdio>
+#include <omp.h>
 
 #define NX 102400
 
@@ -12,10 +13,12 @@ int main(void)
         vecB[i] = vecA[i] * vecA[i];
     }
 
-    /* TODO:
-     *   Implement here a parallelized version of vector addition,
-     *   vecC = vecA + vecB
-     */
+#pragma omp parallel
+    {
+        for (int i = 0; i < NX; i++) {
+            vecC[i] = vecA[i] + vecB[i];
+        }
+    }
 
     double sum = 0.0;
     /* Compute the check value */
