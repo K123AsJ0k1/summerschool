3c131680caab5cf4cb4b6da8a3c27159a9d53385
K123AsJ0k1 <niila.siilasjoki@gmail.com>
2025-06-30 13:28:13 +0300

    ex32

--- gpu/exercises/fundamentals/03-streams/03-asyncmemcopy.cpp
+++ gpu/exercises/fundamentals/03-streams/03-asyncmemcopy.cpp
@@ -50,31 +50,27 @@ int main() {
   HIP_ERRCHK(hipMemcpy(a, d_a, N_bytes/100, hipMemcpyDefault));
   HIP_ERRCHK(hipDeviceSynchronize());
 
-  HIP_ERRCHK(hipMemcpyAsync(d_a, a, N_bytes, hipMemcpyHostToDevice, stream_a));
   kernel_a<<<gridsize, blocksize,0,stream_a>>>(d_a, N);
   HIP_ERRCHK(hipGetLastError());
-  HIP_ERRCHK(hipMemcpyAsync(a, d_a, N_bytes, hipMemcpyDeviceToHost, stream_a));
 
-  HIP_ERRCHK(hipMemcpyAsync(d_b, b, N_bytes, hipMemcpyHostToDevice, stream_b));
   kernel_b<<<gridsize, blocksize,0,stream_b>>>(d_b, N);
   HIP_ERRCHK(hipGetLastError());
-  HIP_ERRCHK(hipMemcpyAsync(b, d_b, N_bytes, hipMemcpyDeviceToHost, stream_b));
 
-  HIP_ERRCHK(hipMemcpyAsync(d_c, c, N_bytes, hipMemcpyHostToDevice, stream_c));
   kernel_c<<<gridsize, blocksize,0,stream_c>>>(d_c, N);
   HIP_ERRCHK(hipGetLastError());
-  HIP_ERRCHK(hipMemcpyAsync(c, d_c, N_bytes, hipMemcpyDeviceToHost, stream_c));
 
-  HIP_ERRCHK(hipStreamDestroy(stream_a));
-  HIP_ERRCHK(hipStreamDestroy(stream_b));
-  HIP_ERRCHK(hipStreamDestroy(stream_c));
-
+  HIP_ERRCHK(hipMemcpyAsync(a, d_a, N_bytes, hipMemcpyDefault, stream_a));
+  HIP_ERRCHK(hipMemcpyAsync(b, d_b, N_bytes, hipMemcpyDefault, stream_b));
+  HIP_ERRCHK(hipMemcpyAsync(c, d_c, N_bytes, hipMemcpyDefault, stream_c));
+  
+  HIP_ERRCHK(hipStreamSynchronize(stream_a));
   for (int i = 0; i < 20; ++i) printf("%f ", a[i]);
   printf("\n");
-
+  HIP_ERRCHK(hipStreamSynchronize(stream_b));
   for (int i = 0; i < 20; ++i) printf("%f ", b[i]);
   printf("\n");
 
+  HIP_ERRCHK(hipStreamSynchronize(stream_c));
   for (int i = 0; i < 20; ++i) printf("%f ", c[i]);
   printf("\n");
 
